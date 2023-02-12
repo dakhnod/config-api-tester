b1725cc1c75c4abb3ef5c21ede4ac516f72d790b
Daniel Dakhno <dakhnod@gmail.com>
2023-02-13 00:12:39 +0100

    added variable replacement logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import requests
 import yaml
 import argparse
+import re
 
 class TestRunner():
     def __init__(self, config, variables={}) -> None:
@@ -26,6 +27,34 @@ class TestRunner():
                 return item
         return RuntimeError(f'no item "{key}" found')
 
+    def string_vars_replace(self, string, variables=None):
+        if variables is None:
+            variables = self.variables
+        vars = re.findall('\\{[^}]+\\}', string)
+        for var in vars:
+            # cut off brackets
+            key = var[1:-1]
+            # cut off leading and trailing spaces
+            key = key.strip()
+            try:
+                value = self.variables[key]
+            except KeyError:
+                raise KeyError(f'variable "{key}" not defined')
+            string = string.replace(var, value)
+        return string
+
+    def vars_replace(self, subject, variables=None):
+        if type(subject) is dict:
+            subject = subject.copy()
+            for key, value in subject.items():
+                subject[key] = self.vars_replace(value)
+        elif type(subject) is list:
+            subject = list(map(self.vars_replace, subject))
+        elif type(subject) in [str, int, float, bool]:
+            return self.string_vars_replace(str(subject))
+
+        return subject 
+
     def find_request(self, key: str):
         return self.find_item(self.requests, key)
 
@@ -46,14 +75,41 @@ class TestRunner():
             # headers=headers
         )
 
-        return {
-            'http_code': response.status_code
+        result = {
+            'http_code': response.status_code,
+            'text': response.text
         }
 
-    def ensure_list(item):
+        return result
+
+    def ensure_list(self, item):
         if type(item) is list:
             return item
         return [item]
+
+    def print_color(self, text, color):
+        colors = {
+            'fail': '\033[91m',
+            'ok': '\033[92m',
+            'end': '\033[0m'
+        }
+        print(f'{colors[color]}{text}{colors["end"]}', end='')
+
+    def print_fail(self, text):
+        self.print_color(text, 'fail')
+
+    def print_ok(self, text):
+        self.print_color(text, 'ok')
+
+    def print_label_fail(self, text):
+        print('[', end='')
+        self.print_fail('FAIL')
+        print(f'] {text}')
+
+    def print_label_ok(self, text):
+        print('[', end='')
+        self.print_ok('SUCCESS')
+        print(f'] {text}')
 
     def run_set(self, set: list|dict):
         set = self.ensure_list(set)
@@ -72,16 +128,21 @@ class TestRunner():
             except KeyError:
                 pass
 
-    def run_test(self, test: str|dict):
+    def dict_get_replaced(self, subject, key, additional_variables={}, default=None):
+        result = subject.get(key)
+        if result is None:
+            return default
+        return self.vars_replace(result, self.variables | additional_variables)
+
+    def run_test(self, test: str|dict, name: None):
         if type(test) == str:
             test = self.find_test(test)
 
-        print(f'running test {test["name"]}')
+        print(f'running test {name}')
 
-        self.run_commands(test.get('before'))
-        try:
-            request = test['request']
-        except KeyError:
+        self.run_commands(self.dict_get_replaced(test, 'before'))
+        request = self.dict_get_replaced(test, 'request')
+        if request is None:
             # no request defined
             return
         if type(request) == 'str':
@@ -95,36 +156,52 @@ class TestRunner():
         response = self.send_request(request)
         
         success = True
-        expect = test.get('expect', {})
-        try:
-            expect['http_code']
-        except KeyError:
-            # default http code
-            expect['http_code'] = 200
+        expect = self.dict_get_replaced(test, 'expect', response, default={})
+        # default value
+        expect = {'http_code': 200} | expect
         for key, value in expect.items():
-            if response[key] != value:
+            if str(response[key]) != value:
                 success = False
-                print(f'key {key} not matching')
+                self.print_label_fail(f'key {key} not matching (expected: {value}, actual: {response[key]})')
 
-        self.run_commands(test.get('after'))
+        if success:
+            self.print_label_ok(f'Test {name}')
+        else:
+            self.print_label_fail(f'Test {name}')
+
+        self.run_commands(self.dict_get_replaced(test, 'after', response))
 
     def run_all_tests(self):
-        for test in self.tests:
-            self.run_test(test)
+        try:
+            for i in range(len(self.tests)):
+                test = self.tests[i]
+                name = self.dict_get_replaced(test, 'name', default=f'Test #{i+1}')
+                self.run_test(test, name)
+        except Exception as e:
+            self.print_label_fail(f'Error running test "{name}": {e}')
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', required=True, type=str, help='Path to test config file')
+    parser.add_argument('-d', '--define', required=False, type=str, help='Initial variable definition', nargs='+', default=[])
 
     args = parser.parse_args()
+
+    env = {}
+
+    for define in args.define:
+        # plit into key=value
+        key = define[:define.find('=')]
+        value = define[define.find('=') + 1:]
+        env[key] = value
 
     file = args.config
 
     with open(file, 'r') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
     
-    runner = TestRunner(config)
+    runner = TestRunner(config, env)
     runner.run_all_tests()
 
 if __name__ == '__main__':
